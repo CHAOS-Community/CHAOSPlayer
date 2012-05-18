eb73aa0a98be79d640e2d3d4470a86c6ab553db1
Jacob Poul Richardt <jacob@geckon.com>
2012-05-18 21:19:32 +0200

    Added possibility to set session to be used. Added login form to example.

--- src/CHAOSPlayer.js
+++ src/CHAOSPlayer.js
@@ -1,79 +1,87 @@
-﻿var settings = $.deparam.fragment();
-var documentIsReady = false;
-var videoPath = null;
-var thumbPath = null;
-var client = null;
-
-if(settings.objectGUID)
-	client = new PortalClient("http://api.chaos-systems.com", "DCC822C8-AFD9-4E8C-90BE-06962111A398");
-else
-	ReportError("objectGUID not set");
-
-client.SessionCreated().Add(function ()
+﻿(function()
 {
-	client.EmailPassword_Login(null, "", ""); 
-});
+	var clientGUID = "DCC822C8-AFD9-4E8C-90BE-06962111A398";
 
-client.SessionAuthenticated().Add(function ()
-{
-	client.Object_GetByObjectGUID(ClientGetObjectByGUIDCallback, settings.objectGUID, false, true, false);
-});
+	var documentIsReady = false;
+	var videoPath = null;
+	var thumbPath = null;
+	var client = null;
 
-$(document).ready(function (event)
-{
-	documentIsReady = true;
-	AddPlayer();
-});
+	var settings = $.deparam.fragment();
 
-function ClientGetObjectByGUIDCallback(serviceResult)
-{
-	if(serviceResult.WasSuccess() && serviceResult.MCM().WasSuccess())
+	$(document).ready(function (event)
 	{
-		var objects = serviceResult.MCM().Results();
+		documentIsReady = true;
+		AddPlayer();
+	});
 
-		if(objects.length != 0)
+	if(settings.objectGUID)
+	{
+		var sessionGUIDSet = typeof settings.sessionGUID != "undefined";
+		
+		client = new PortalClient(settings.servicePath, clientGUID, !sessionGUIDSet);
+
+		client.SessionAcquired().Add(function ()
 		{
-			object = objects[0];
-			
-			for(var i = 0; i < object.Files.length; i++)
-			{
-				switch(filePath = object.Files[i].Format)
-				{
-					case "SMK video":
-						videoPath = object.Files[i].URL;
-						break;
-					case "SMK asset thumbnail":
-						thumbPath = object.Files[i].URL;
-						break;
-				}
-			}
-
-			AddPlayer();
-		}
-		else
-			ReportError("Object not found");
+			client.Object_GetByObjectGUID(ClientGetObjectByGUIDCallback, settings.objectGUID, false, true, false);
+		});
+		
+		if(sessionGUIDSet)
+			client.SetSessionGUID(settings.sessionGUID);
 	}
 	else
+		ReportError("objectGUID not set");
+
+	function ClientGetObjectByGUIDCallback(serviceResult)
 	{
-		ReportError("Service error: " + (serviceResult.WasSuccess() ? serviceResult.MCM().Error() : serviceResult.Error()));
+		if(serviceResult.WasSuccess() && serviceResult.MCM().WasSuccess())
+		{
+			var objects = serviceResult.MCM().Results();
+
+			if(objects.length != 0)
+			{
+				object = objects[0];
+
+				for(var i = 0; i < object.Files.length; i++)
+				{
+					switch(object.Files[i].FormatType)
+					{
+						case "Video":
+							videoPath = object.Files[i].URL;
+							break;
+						case "Image":
+							thumbPath = object.Files[i].URL;
+							break;
+					}
+				}
+
+				AddPlayer();
+			}
+			else
+				ReportError("Object not found");
+		}
+		else
+		{
+			ReportError("Service error: " + (serviceResult.WasSuccess() ? serviceResult.MCM().Error() : serviceResult.Error()));
+		}
 	}
-}
 
-function AddPlayer()
-{
-	if(videoPath == null || !documentIsReady)
-		return;
+	function AddPlayer()
+	{
+		if(videoPath == null || !documentIsReady)
+			return;
 
-	jwplayer("PlayerContainer").setup({
-		flashplayer: "../lib/jwplayer/player.swf",
-		file: videoPath,
-		image: thumbPath,
-		height: "100%",
-		width: "100%"
-	});
-}
+		jwplayer("PlayerContainer").setup({
+			flashplayer: "../lib/jwplayer/player.swf",
+			file: videoPath,
+			image: thumbPath,
+			height: "100%",
+			width: "100%"
+		});
+	}
 
-function ReportError(message)
-{
-	$("#PlayerContainer").text(message);
-}
+	function ReportError(message)
+	{
+		$("#PlayerContainer").text(message);
+	}
+})();
